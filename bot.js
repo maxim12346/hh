2a1f038ee095158304d70c03b936a3d96210ca92
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-22 22:04:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,60 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+  	}
 });
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-    if(command === settings.prefix + settings.rainbowcommand) {
-        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
-        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
-        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
-        var colors = settings.rainbowrole
-        var rolestart = setInterval(function() {
-            var colorsz = colors[Math.floor(Math.random() * colors.length)];
-            rolez.setColor(colorsz)
-        }, settings.rainbowdelay); 
-            message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
 
-    }
-    if(command === settings.prefix + settings.rainbowstop) {
-            setTimeout(function () {
-           process.exit()
-            }, 5000);
-            message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
-        }
-    
-        if(command == '=help')
-               {
-                   message.reply("вот помощь: \n***главное***\n**=rainbow @роль** - \`запустить изменение роли., \n=invite - пригласить бота на сервер, \n =stop `отключить изменение роли `");
-                   
-               }
-        if(command == '=invite')
-               {
-                   message.reply("пригласить бота: https://discordapp.com/oauth2/authorize?client_id=557279829804711941&scope=bot&permissions=2146958591\`");
-   
-               }
-               if(command == '=8ball')
-               {
-               var fortunes = [
-                   "Без сомннения!",
-                   "да",
-                   "без сомнения!",
-                   "неа",
-                   "не совсем",
-                   "абсолютно!",
-                   "абсолютно нет",
-                   "нет",
-                   "неееет",
-                   "я вообще не знаю.."
-               ]
-               if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
-            }
-            });
-   //{         
-                         
-    //   message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"));
-    //                      }
-            //});           
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
