86fdb7d96626ac36d2fa5215ee9205e227dffe6a
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-22 20:38:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,43 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '=help') {
+    	message.reply("вот помощь: \n***главное***\n**=rainbow @роль** - \`запустить изменение роли., \n=invite - пригласить бота на сервер, \n =stop `отключить изменение роли `);
   	}
-});
+});   
+      let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rainbowcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.rainbowrole
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
 
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            setTimeout(function () {
+           process.exit()
+            }, 5000);
+            message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+            if(command == '=invite')
+               {
+                   message.reply("пригласить бота: https://discordapp.com/oauth2/authorize?client_id=557279829804711941&scope=bot&permissions=2146958591\`");
+   
+               }
+           //{         
+                         
+    //   message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"));
+    //                      }
+            //});
+           
+                       
+                       
+//});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
