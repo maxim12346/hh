27dfb6d8560b5adf0c8ae8a91bf3224b753617ae
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-23 21:38:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,15 @@
 const Discord = require("discord.js") 
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
 const settings = require("./your_settings.json")
 const client = new Discord.Client()
 client.on('ready', async => {
