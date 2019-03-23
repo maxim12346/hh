1c2ebf4c21ffc0781179bb171ede25867bcc8ce9
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-23 21:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,49 +1,43 @@
 const Discord = require("discord.js") 
 const settings = require("./your_settings.json")
-const client = new Discord.Client()
-client.on('ready', async => {
-console.log("Rainbow bot is ready!" + "\n" + client.user.tag + "\n" + "Server Count: "  + client.guilds.size + "\n" + "Cached users: " + client.users.size + "\n" + "Enjoy!")
+const bot = new Discord.Client()
+bot.on('ready', async => {
+console.log("Rainbow bot is ready!" + "\n" + bot.user.tag + "\n" + "Server Count: "  + bot.guilds.size + "\n" + "Cached users: " + bot.users.size + "\n" + "Enjoy!")
 });
-client.on('message', message => {
-    const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-    //const poll = message.content.slice(prefix.length).trim().split(/;+/g);
-    const command = args.shift().toLowerCase();
-    if(message.user.bot || ! message.content.startsWith(settings.prefix)) return;
-    if(command === settings.rainbowcommand) {
-        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args[0])
+bot.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rainbowcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
         if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
-        if(!message.guild.member(client.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        if(!message.guild.member(bot.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
         var colors = settings.rainbowrole
         var rolestart = setInterval(function() {
             var colorsz = colors[Math.floor(Math.random() * colors.length)];
-            rolez.setColor(colorsz).catch();
+            rolez.setColor(colorsz)
         }, settings.rainbowdelay); 
             message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
 
     }
-    if(command === settings.rainbowstop) {
+    if(command === settings.prefix + settings.rainbowstop) {
             setTimeout(function () {
            process.exit()
             }, 5000);
             message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
         }
     
-        if(command == 'help')
+        if(command == '=help')
                {
                    message.reply("вот помощь: \n***главное***\n**=rainbow @роль** - \`запустить изменение роли., \n=invite - пригласить бота на сервер, \n =stop `отключить изменение роли `");
                    
                }
-        if(command == 'invite')
+        if(command == '=invite')
                {
-                   message.reply(`пригласить бота: \`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=2146958591\``);
+                   message.reply("пригласить бота: https://discordapp.com/oauth2/authorize?client_id=557279829804711941&scope=bot&permissions=2146958591\`");
    
                }
-               if(command == '8ball')
-
-
-
-
-
+               if(command == '=8ball')
                {
                var fortunes = [
                    "Без сомннения!",
@@ -57,7 +51,7 @@ client.on('message', message => {
                    "неееет",
                    "я вообще не знаю.."
                ]
-               if (args[0]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+               if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
             }
             });
    //{         
@@ -69,5 +63,4 @@ client.on('message', message => {
                        
                        
 //});
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(settings.token).catch(err=> console.log("Incorrect Token was provided"))
