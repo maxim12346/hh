7bf1483ef286a8ba1f4d63502aefc88120b134eb
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-23 17:25:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client()
 client.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + client.user.tag + "\n" + "Server Count: "  + client.guilds.size + "\n" + "Cached users: " + client.users.size + "\n" + "Enjoy!")
 });
-bot.on('message', message => {
+client.on('message', message => {
     const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
     //const poll = message.content.slice(prefix.length).trim().split(/;+/g);
     const command = args.shift().toLowerCase();
