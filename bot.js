af14c719732ac653c718bce89d9535c9dfc03134
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-25 21:28:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,7 @@
 const Discord = require("discord.js") 
 const settings = require("./your_settings.json")
+const client = new Discord.Client()
+client.on('ready', async => {
+console.log("Rainbow bot is ready!" + "\n" + client.user.tag + "\n" + "Server Count: "  + client.guilds.size + "\n" + "Cached users: " + client.users.size + "\n" + "Enjoy!")
+});
+bot.on('message', message => {
