a383b3c2c84b0fbe4853a65442bb271505f44618
maxim12346 <48769047+maxim12346@users.noreply.github.com>
2019-03-25 21:29:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,3 +5,9 @@ client.on('ready', async => {
 console.log("Rainbow bot is ready!" + "\n" + client.user.tag + "\n" + "Server Count: "  + client.guilds.size + "\n" + "Cached users: " + client.users.size + "\n" + "Enjoy!")
 });
 bot.on('message', message => {
+      const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+    //const poll = message.content.slice(prefix.length).trim().split(/;+/g);
+    const command = args.shift().toLowerCase();
+    if(message.user.bot || ! message.content.startsWith(settings.prefix)) return;
+    if(command === settings.rainbowcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args[0])
